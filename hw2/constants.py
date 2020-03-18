bbd143166ed9e5fa3b96d4815524c4a5448969ea
Jacky Liang <jackyliang@cmu.edu>
2020-03-18 09:38:51 -0400

    working notebook skeleton

---
+++ hw2/constants.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+CONSTS = {
+    'x_lim': np.array([-10, 10]),
+    'y_lim': np.array([-10, 10]),
+    'theta_lim': np.array([-np.pi, np.pi]),
+    'xp_lim': np.array([-15, 15]),
+    'yp_lim': np.array([-15, 15]),
+    'num_waypoints': 10,
+    'waypoint_tol': 0.2,
+    'wheelbase': 0.335,
+    'max_acc': 3,
+    'max_steering_angle': 0.5
+}
---
+++ source_hw2.sh
@@ -0,0 +1,3 @@
+deactivate
+source /opt/ros/kinetic/setup.bash
+source devel/setup.bash
