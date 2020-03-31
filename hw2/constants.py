97df887554823d9ead48317a999bca56b090573c
Jacky Liang <jackyliang@cmu.edu>
2020-03-31 10:54:37 -0400

    wip torch

--- hw2/constants.py
+++ hw2/constants.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-CONSTS = {
+CS = {
     'x_lim': np.array([-10, 10]),
     'y_lim': np.array([-10, 10]),
     'theta_lim': np.array([-np.pi, np.pi]),
@@ -9,7 +9,10 @@ CONSTS = {
     'yp_lim': np.array([-15, 15]),
     'num_waypoints': 10,
     'waypoint_tol': 0.2,
+    'angle_tol': 0.2,
     'wheelbase': 0.335,
     'max_acc': 3,
-    'max_steering_angle': 0.5
+    'max_steering_angle': 0.5,
+    'max_vel': 10
 }
+
---
+++ hw2/torch_utils.py
@@ -0,0 +1,119 @@
+import os
+import random
+import numpy as np
+import torch
+
+from torch.autograd import Function
+import scipy.linalg
+
+
+# From https://github.com/steveli/pytorch-sqrtm
+class _MatrixSquareRoot(Function):
+    """Square root of a positive definite matrix.
+    NOTE: matrix square root is not differentiable for matrices with
+          zero eigenvalues.
+    """
+    @staticmethod
+    def forward(ctx, m):
+        m = get_numpy(m)
+        sqrtm = from_numpy(scipy.linalg.sqrtm(m).real)
+        ctx.save_for_backward(sqrtm)
+        return sqrtm
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        grad_input = None
+        if ctx.needs_input_grad[0]:
+            sqrtm, = ctx.saved_tensors
+            sqrtm = get_numpy(sqrtm)
+            gm = get_numpy(grad_output)
+
+            # Given a positive semi-definite matrix X,
+            # since X = X^{1/2}X^{1/2}, we can compute the gradient of the
+            # matrix square root dX^{1/2} by solving the Sylvester equation:
+            # dX = (d(X^{1/2})X^{1/2} + X^{1/2}(dX^{1/2}).
+            grad_sqrtm = scipy.linalg.solve_sylvester(sqrtm, sqrtm, gm)
+            grad_input = from_numpy(grad_sqrtm)
+        return grad_input
+
+
+sqrtm = _MatrixSquareRoot.apply
+
+
+def weight_init(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+        nn.init.xavier_uniform_(m.weight.data)
+
+
+def set_seed(seed, do_torch=False):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)    
+    np.random.seed(seed)
+
+    if do_torch:
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+
+def batch_apply(f, X):
+    return torch.stack([f(x) for x in X])
+
+
+"""
+GPU wrappers from 
+https://github.com/vitchyr/rlkit/blob/master/rlkit/torch/pytorch_util.py
+"""
+
+_use_gpu = False
+device = None
+
+def set_gpu_mode(mode, gpu_id=0):
+    global _use_gpu
+    global device
+    global _gpu_id
+    _gpu_id = gpu_id
+    _use_gpu = mode
+    device = torch.device("cuda:" + str(gpu_id) if _use_gpu else "cpu")
+
+def gpu_enabled():
+    return _use_gpu
+
+def set_device(gpu_id):
+    torch.cuda.set_device(gpu_id)
+
+# noinspection PyPep8Naming
+def FloatTensor(*args, **kwargs):
+    return torch.FloatTensor(*args, **kwargs).to(device)
+
+def tensor(*args, **kwargs):
+    return torch.tensor(*args, **kwargs).float().to(device)
+
+def from_numpy(*args, **kwargs):
+    return torch.from_numpy(*args, **kwargs).float().to(device)
+
+def get_numpy(tensor):
+    # not sure if I should do detach or not here
+    return tensor.to('cpu').detach().numpy()
+
+def zeros(*sizes, **kwargs):
+    return torch.zeros(*sizes, **kwargs).to(device)
+
+def ones(*sizes, **kwargs):
+    return torch.ones(*sizes, **kwargs).to(device)
+
+def eye(*sizes, **kwargs):
+    return torch.eye(*sizes, **kwargs).to(device)
+
+def randn(*args, **kwargs):
+    return torch.randn(*args, **kwargs).to(device)
+
+def rand(*args, **kwargs):
+    return torch.rand(*args, **kwargs).to(device)
+
+def zeros_like(*args, **kwargs):
+    return torch.zeros_like(*args, **kwargs).to(device)
+
+def normal(*args, **kwargs):
+    return torch.normal(*args, **kwargs).to(device)
